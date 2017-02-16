89a5dc38919b201636939dfb5f28451891469eb7
Jose Antonio Iñigo Garrido <jainigo@profile.es>
2017-02-16 16:41:52 +0100

    initial commit

---
+++ secure-server/src/main/java/com/codependent/mutualauth/SecureServerApplication.java
@@ -0,0 +1,12 @@
+package com.codependent.mutualauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureServerApplication.class, args);
+	}
+}
---
+++ secure-server/src/main/java/com/codependent/mutualauth/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.codependent.mutualauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+			.and()
+				.x509()
+					.subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+					.userDetailsService(userDetailsService());
+	}
+	
+	@Bean
+    public UserDetailsService userDetailsService() {
+        return (username -> {
+        	if (username.equals("codependent-client")) {
+                return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+            }else{
+            	return null;
+            }
+        });
+    }
+	
+}
